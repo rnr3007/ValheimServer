90411c75bf16ca4ec8004bddfd6cd101b7714f12
Riva N <rivarahim@gmail.com>
2025-11-08 11:13:21 +0700

    [fix] Server Start

--- StartValheim.sh
+++ StartValheim.sh
@@ -1,25 +1,31 @@
-#!/bin/bash 
+#!/bin/sh
 
 # Default path if not provided
 server_path=$1
-if [[ -z "$server_path" ]]; then
+if [ -z "$server_path" ]; then
     server_path=$HOME/steam/valheim
 fi
+
+# Copy the InginTuru.db to valheim_world directory
+if [ ! -e "$HOME/.config/unity3d/IronGate/Valheim/worlds_local/InginTuru.db" ]; then
+    cp ./valheim_world/InginTuru.db $HOME/.config/unity3d/IronGate/Valheim/worlds_local/InginTuru.db
+fi
+
+# Copy the InginTuru.fwl to valheim_world directory
+if [ ! -e "$HOME/.config/unity3d/IronGate/Valheim/worlds_local/InginTuru.fwl" ]; then
+    cp ./valheim_world/InginTuru.fwl $HOME/.config/unity3d/IronGate/Valheim/worlds_local/
+fi
+
+echo "Starting server PRESS CTRL-C to exit"  
+cd $server_path
 
 export templdpath=$LD_LIBRARY_PATH  
 export LD_LIBRARY_PATH=$server_path/linux64:$LD_LIBRARY_PATH  
 export SteamAppID=892970
 
-# Copy the InginTuru.db to valheim_world directory
-if [[ ! -e "$HOME/.config/unity3d/IronGate/Valheim/worlds_local/InginTuru.db" ]]; then
-    cp ./valheim_world/InginTuru.db $HOME/.config/unity3d/IronGate/Valheim/worlds_local/InginTuru.db
-fi
+nohup ./valheim_server.x86_64 -name "InginTuruNew" -port 2456 -nographics -batchmode -world "InginTuru" -password "HuluSiaBitu" -public 1 >> ./ValheimServer.log &
 
-# Copy the InginTuru.fwl to valheim_world directory
-if [[ ! -e "$HOME/.config/unity3d/IronGate/Valheim/worlds_local/InginTuru.fwl" ]]; then
-    cp ./valheim_world/InginTuru.fwl $HOME/.config/unity3d/IronGate/Valheim/worlds_local/
-fi
-
-echo "Starting server PRESS CTRL-C to exit"  
-nohup bash $server_path/valheim_server.x86_64 -name "InginTuruNew" -port 2456 -nographics -batchmode -world "InginTuru" -password "HuluSiaBitu" -public 1 >> ./ValheimServer.log &
 export LD_LIBRARY_PATH=$templdpath
+
+echo "Entering ValheimServer Log, use CTRL-C to exit"
+tail -f ./ValheimServer.log
